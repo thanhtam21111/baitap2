dccfe2deb4d1091429f454db3cc11b0a31c4a8ed
giangminhhoang999 <giangminhhoang999@gmail.com>
2018-12-24 16:24:02 +0700

    aaa

---
+++ btquanlinhanvien/btquanlinhanvien/nhan vien.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: C6-A04
+ * Date: 12/24/2018
+ * Time: 2:52 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace btquanlinhanvien
+{
+	/// <summary>
+	/// Description of nhan_vien.
+	/// </summary>
+	public class nhan_vien
+	{
+		public nhan_vien()
+		{
+		}
+	}
+}
